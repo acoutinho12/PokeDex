dce1ac817b993cad694e159d6df80bb9cbb370dd
Andre <andre.coutinho@bernoulli.com.br>
2021-05-27 20:10:13 -0300

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Andre Luis Barbosa Coutinho on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
